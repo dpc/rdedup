4604b5ee6bfda2befd8c4238181e3baaf9ee490f
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-03-25 19:00:00 -0700

    Fix data finish, and rolling sum reset on chunk edge.

--- src/bin.rs
+++ src/bin.rs
@@ -8,16 +8,82 @@ use rollsum::Engine;
 use crypto::sha2;
 use crypto::digest::Digest;
 
+enum Message {
+    Data(Vec<u8>),
+    ChunkEdge(Vec<u8>), // Sha256 hash of the data sent since the last ChunkEdge
+}
+
+struct Chunker {
+    roll : rollsum::Bup,
+    sha256 : sha2::Sha256,
+    bytes_total : usize,
+    bytes_chunk: usize,
+    chunks_total : usize,
+}
+
+impl Chunker {
+    pub fn new() -> Self {
+        Chunker {
+            roll: rollsum::Bup::new(),
+            sha256: sha2::Sha256::new(),
+            bytes_total: 0,
+            bytes_chunk: 0,
+            chunks_total: 0,
+        }
+    }
+
+    pub fn chunk_found(&mut self) {
+
+        println!("found edge at {}; sum: {:x}",
+                 self.bytes_total,
+                 self.roll.digest());
+
+        println!("sha256 hash: {}",
+                 self.sha256.result_str());
+
+        self.chunks_total += 1;
+        self.bytes_chunk += 0;
+
+        self.sha256.reset();
+        self.roll = rollsum::Bup::new();
+    }
+
+    pub fn feed(&mut self, buf : &[u8]) {
+        let mut ofs : usize = 0;
+        let len = buf.len();
+        while ofs < len {
+            if let Some(count) = self.roll.find_chunk_edge(&buf[ofs..len]) {
+                self.sha256.input(&buf[ofs..ofs+count]);
+
+                ofs += count;
+
+                self.bytes_chunk += count;
+                self.bytes_total += count;
+                self.chunk_found();
+            } else {
+                let count = len - ofs;
+                self.sha256.input(&buf[ofs..len]);
+                self.bytes_chunk += count;
+                self.bytes_total += count;
+                break
+            }
+        }
+    }
+
+    pub fn finish(&mut self) {
+        if self.bytes_chunk != 0 {
+            self.chunk_found();
+        }
+    }
+}
+
 fn main() {
     let mut stdin = io::stdin();
 
     let mut buf = [0u8; 16 * 1024];
 
-    let mut total_ofs = 0usize;
-    let mut total_chunks = 0usize;
 
-    let mut b = rollsum::Bup::new();
-    let mut sha256 = sha2::Sha256::new();
+    let mut chunker = Chunker::new();
 
     loop {
         let len = stdin.read(&mut buf).unwrap();
@@ -26,23 +92,8 @@ fn main() {
             break;
         }
 
-        let mut ofs : usize = 0;
-        while ofs < len {
-            if let Some(count) = b.find_chunk_edge(&buf[ofs..len]) {
-                println!("found edge at {}; sum: {:x}", total_ofs + ofs + count, b.digest());
-                sha256.input(&mut buf[ofs..ofs+count]);
-                println!("sha256 hash: {}", sha256.result_str());
-                sha256.reset();
-
-                ofs += count;
-                total_chunks += 1;
-            } else {
-                sha256.input(&mut buf[ofs..len]);
-                break
-            }
-        }
-
-        total_ofs += len;
+        chunker.feed(&buf[..len]);
     }
-    println!("chunks found: {}", total_chunks);
+    chunker.finish();
+    println!("chunks found: {}", chunker.chunks_total);
 }
