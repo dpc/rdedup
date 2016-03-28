1767f20c60723d6f7fa738bfb6ff3740a60f4971
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-03-27 20:36:50 -0700

    Progress.

--- src/bin.rs
+++ src/bin.rs
@@ -2,11 +2,12 @@ extern crate rollsum;
 extern crate crypto;
 #[macro_use]
 extern crate log;
+extern crate rustc_serialize as serialize;
 
-use std::io;
-use std::io::Read;
-use std::mem;
-use std::thread;
+use std::io::{Read, Write};
+use std::{fs, mem, thread, io};
+use std::path::{Path, PathBuf};
+use serialize::hex::ToHex;
 
 use std::sync::mpsc;
 
@@ -31,7 +32,6 @@ struct Chunker {
 
     edges : Vec<Edge>,
 }
-
 
 impl Chunker {
     pub fn new() -> Self {
@@ -96,10 +96,55 @@ impl Chunker {
     }
 }
 
-fn stdio_chunker(tx : mpsc::Sender<Message>) {
+fn write_with_io<F>(tx : mpsc::Sender<Message>, mut input_f : F)
+where F : FnMut() -> Vec<u8> {
     let mut stdin = io::stdin();
     let mut chunker = Chunker::new();
 
+    let mut index : Vec<u8> = vec!();
+    loop {
+        let mut buf = input_f();
+        let len = stdin.read(&mut buf).unwrap();
+
+        if len == 0 {
+            break;
+        }
+
+        let edges = chunker.input(&buf[..len]);
+
+        for &(_, ref sum) in &edges {
+            index.append(&mut sum.clone());
+        }
+        tx.send(Message::Data(buf, edges)).unwrap();
+    }
+    let edges = chunker.finish();
+
+    for &(_, ref sum) in &edges {
+        index.append(&mut sum.clone());
+    }
+    tx.send(Message::Data(vec!(), edges)).unwrap();
+    tx.send(Message::Exit).unwrap();
+
+    println!("index size: {}", index.len());
+    println!("chunks found: {}", chunker.chunks_total);
+}
+
+fn write_stdio(tx : mpsc::Sender<Message>) {
+    let mut stdin = io::stdin();
+    write_with_io(tx, || {
+        let mut buf = vec![0u8; 16 * 1024];
+        let len = stdin.read(&mut buf).unwrap();
+        buf.truncate(len);
+        buf
+    });
+}
+
+/*
+fn read_stdio(tx : mpsc::Sender<Message>) {
+    let mut stdin = io::stdin();
+    let mut chunker = Chunker::new();
+
+    let mut index : Vec<u8> = vec!();
     loop {
         let mut buf = vec![0u8; 16 * 1024];
         let len = stdin.read(&mut buf).unwrap();
@@ -110,21 +155,69 @@ fn stdio_chunker(tx : mpsc::Sender<Message>) {
 
         let edges = chunker.input(&buf[..len]);
 
+        for &(_, ref sum) in &edges {
+            index.append(&mut sum.clone());
+        }
         tx.send(Message::Data(buf, edges)).unwrap();
     }
     let edges = chunker.finish();
+
+    for &(_, ref sum) in &edges {
+        index.append(&mut sum.clone());
+    }
     tx.send(Message::Data(vec!(), edges)).unwrap();
     tx.send(Message::Exit).unwrap();
 
+    println!("index size: {}", index.len());
     println!("chunks found: {}", chunker.chunks_total);
 }
+*/
 
-fn chunk_writer(rx : mpsc::Receiver<Message>) {
+fn digest_to_path(digest : &[u8]) -> PathBuf {
+    Path::new(&digest[0..1].to_hex()).join(digest[1..2].to_hex()).join(&digest.to_hex())
+}
 
+fn chunk_writer(dest_base_dir : &Path, rx : mpsc::Receiver<Message>) {
+    let mut pending_data = vec!();
     loop {
         match rx.recv().unwrap() {
-            Message::Exit => return,
-            _ => {}
+            Message::Exit => {
+                assert!(pending_data.is_empty());
+                return
+            }
+            Message::Data(data, edges) => if edges.is_empty() {
+                println!("0 edges");
+                pending_data.push(data)
+            } else {
+                println!("{} edges", edges.len());
+                let mut prev_ofs = 0;
+                for &(ref ofs, ref sha256) in &edges {
+                    let path = digest_to_path(&sha256);
+                    let path = dest_base_dir.join(path);
+                    println!("Would write {:?}", path);
+                    if !path.exists() {
+                        fs::create_dir_all(path.parent().unwrap()).unwrap();
+                        let mut chunk_file = fs::File::create(path).unwrap();
+
+                        for data in pending_data.drain(..) {
+                            chunk_file.write(&data).unwrap();
+                        }
+
+                        if *ofs != prev_ofs {
+                            chunk_file.write(&data[prev_ofs..*ofs]).unwrap();
+                        }
+                    } else {
+                        pending_data.clear();
+                    }
+                    debug_assert!(pending_data.is_empty());
+
+                    prev_ofs = *ofs;
+                }
+                if prev_ofs != data.len() {
+                    let mut data = data;
+                    pending_data.push(data.split_off(prev_ofs))
+                }
+            }
         }
     }
 }
@@ -132,9 +225,9 @@ fn chunk_writer(rx : mpsc::Receiver<Message>) {
 fn main() {
     let (tx, rx) = mpsc::channel();
 
-    let chunk_writer_join = thread::spawn(|| chunk_writer(rx));
+    let chunk_writer_join = thread::spawn(|| chunk_writer(Path::new("chunks"), rx));
 
-    stdio_chunker(tx);
+    write_stdio(tx);
 
     chunk_writer_join.join().unwrap();
 }
