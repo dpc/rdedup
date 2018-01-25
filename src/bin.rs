38b94a08a90aa66d33de7d548c619df926109d21
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-01-24 22:50:18 -0800

    Add warning about unstable version

--- src/bin.rs
+++ src/bin.rs
@@ -512,6 +512,8 @@ fn run() -> io::Result<()> {
 }
 
 fn main() {
+    eprintln!("Warning! This is an unstable version of `rdedup`.");
+    eprintln!("Check github README to install last stable version.");
     if let Err(e) = run() {
         eprintln!("Error: {}", e);
         process::exit(-1);
